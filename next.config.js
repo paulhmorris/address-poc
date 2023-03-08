109a7b5a3c155b848909049ef7bff5cd2696a99c
paulhmorris <paulh.morris@gmail.com>
2023-03-08 10:55:19 -0600

    Set iframe CSP header

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: {
+    "Content-Security-Policy":
+      "frame-ancestors https://address-iframe.onrender.com",
+  },
 };
 
 module.exports = nextConfig;
