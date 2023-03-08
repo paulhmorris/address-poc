b68d55595df9fbf14b273568b95906cbadcbcfae
paulhmorris <paulh.morris@gmail.com>
2023-03-08 12:13:23 -0600

    Remove headers

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors https://address-iframe.onrender.com/",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://address-iframe.onrender.com",
-          },
-          {
-            key: "Vary",
-            value: "Origin",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
